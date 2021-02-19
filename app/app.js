11f659810bbd6aba51366fde03b49fc5d7567619
KevinitoRyananda <52045419+KevinitoRyananda@users.noreply.github.com>
2021-02-19 22:58:45 +0700

    Add files via upload

--- app/app.js
+++ app/app.js
@@ -1,9 +1,11 @@
 function myFunction() {
   var x = document.getElementById("myLinks");
+  var y = document.getElementById("mobile-logo");
   if (x.style.display === "block") {
     x.style.display = "none";
+    y.style.marginTop = "0px";
   } else {
     x.style.display = "block";
+    y.style.marginTop = "-27px";
   }
-  var y = document.getElementById("myLinks");
 }
