4b1d35417140528c8a51fb23682718337fcf894b
KevinitoRyananda <52045419+KevinitoRyananda@users.noreply.github.com>
2021-02-19 23:16:05 +0700

    Add files via upload

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 function myFunction() {
   var x = document.getElementById("myLinks");
-  var y = document.getElementById("mobile-logo");
+  var y = document.getElementById("mobile-img");
   if (x.style.display === "block") {
     x.style.display = "none";
     y.style.marginTop = "0px";
