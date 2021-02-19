56f0cffb87118c23fd39787dd4370dfd850cfd7d
KevinitoRyananda <52045419+KevinitoRyananda@users.noreply.github.com>
2021-02-20 01:34:29 +0700

    Add files via upload

--- app/app.js
+++ app/app.js
@@ -1,12 +1,15 @@
-alert('Maaf kalau masih kurang rapih, masih belajar hehe :)');
+//alert('Maaf kalau masih kurang rapih, masih belajar hehe :)');
 function myFunction() {
   var x = document.getElementById("myLinks");
   var y = document.getElementById("mobile-img");
+  var a = document.getElementById("bodi");
   if (x.style.display === "block") {
     x.style.display = "none";
     y.style.marginTop = "0px";
+    a.style.overflow = "auto";
   } else {
     x.style.display = "block";
     y.style.marginTop = "-27px";
+    a.style.overflow = "hidden";
   }
 }
