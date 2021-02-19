1bad3d1d777eb73007b22d72a78011777b8c610b
KevinitoRyananda <52045419+KevinitoRyananda@users.noreply.github.com>
2021-02-20 00:39:31 +0700

    Add files via upload

--- app/app.js
+++ app/app.js
@@ -1,3 +1,4 @@
+alert('Maaf kalau masih kurang rapih, masih belajar hehe :)');
 function myFunction() {
   var x = document.getElementById("myLinks");
   var y = document.getElementById("mobile-img");
