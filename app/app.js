930851857b43e444fb6311099922db9dd4b6b989
KevinitoRyananda <52045419+KevinitoRyananda@users.noreply.github.com>
2021-02-26 21:11:14 +0700

    Add files via upload

--- app/app.js
+++ app/app.js
@@ -1,9 +1,21 @@
-alert('works on 1440 and mobile');
+//alert('works on 1440 and mobile');
 
+function scroll(){
+  var x = document.getElementById("myLinks");
+  var y = document.getElementById("mobile-img");
+  var a = document.getElementById("bodi");
+  console.log("CLICKED")
+  x.style.display = "none";
+  y.style.marginTop = "0px";
+  a.style.overflow = "auto";
+}
 function myFunction() {
   var x = document.getElementById("myLinks");
   var y = document.getElementById("mobile-img");
   var a = document.getElementById("bodi");
+  document.getElementById("view-plan").onclick=scroll;
+  document.getElementById("footers").onclick=scroll;
+  document.getElementById("how-we-works").onclick=scroll;
   if (x.style.display === "block") {
     x.style.display = "none";
     y.style.marginTop = "0px";
